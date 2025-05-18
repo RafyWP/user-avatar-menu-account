7271e6ab129609eb86e9c6e49026c54f5482321f
RafyWP <rafy@rafy.com.br>
2025-05-18 18:12:50 -0300

    Standardizing plugin data

--- user-avatar-menu-account.php
+++ user-avatar-menu-account.php
@@ -4,32 +4,30 @@
  * 
  * Displays the logged-in user's avatar (or icon), display name, and a customizable link (which can serve as a 'My Account' link if WooCommerce is active).
  * 
- * @link              https://litcode.store/product/user-avatar-menu-account-wordpress-block-plugin/
+ * @link              https://rafy.com.br/project/user-avatar-menu-account
  * @since             1.0.0
  * @package           rafy
- * @author            Lit ✴ Code
+ * @author            Rafy Co.
  * @license           GPL-2.0-or-later
  * 
  * @wordpress-plugin
  * Plugin Name:       User Avatar & Menu Account
- * Plugin URI:        https://litcode.store/product/user-avatar-menu-account-wordpress-block-plugin/
+ * Plugin URI:        https://rafy.com.br/project/user-avatar-menu-account
  * Description:       Displays the logged-in user's avatar (or icon), display name, and a customizable link (which can serve as a 'My Account' link if WooCommerce is active).
  * Version:           1.0.0
  * Requires at least: 6.7
  * Requires PHP:      7.4
- * Author:            Lit ✴ Code
- * Author URI:        https://litcode.store
+ * Author:            Rafy Co.
+ * Author URI:        https://rafy.com.br
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       user-avatar-menu-account
  * Domain Path:       /languages
- * Update URI:        https://github.com/RafyWP/user-avatar-menu-account
+ * Update URI:        https://github.com/RafyWP/user-avatar-menu-account/tree/master
  * Network:           true
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
-}
+defined( 'ABSPATH' ) || exit;
 
 /**
  * Generates class names and styles to apply
@@ -62,6 +60,8 @@ function rafy_block_user_avatar_menu_account_border_attributes( $attributes ) {
  * Registers the block using the metadata loaded from the `block.json` file.
  * Behind the scenes, it registers also all assets so they can be enqueued
  * through the block editor in the corresponding context.
+ * 
+ * @since 1.0.0
  *
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
